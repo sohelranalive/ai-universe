92658ac0a7770aa597da032d40f2afe56d4693ed
sohelranalive <sohel.cse37@gmail.com>
2023-03-03 02:35:13 +0600

    first page style & functionality complete. need to work on modal

--- js/main.js
+++ js/main.js
@@ -18,35 +18,35 @@ const showContent = (data, contentCount) => {
     }
 
     data.forEach(singleData => {
-        // console.log(singleData);
-        const { id, image, features, name, published_in } = singleData
-        const singleDataContainer = document.createElement('div');
+        
+        const { id, image, name, features, published_in } = singleData;
+ const singleDataContainer = document.createElement('div');
         singleDataContainer.classList.add('col');
         singleDataContainer.innerHTML =
-            `<div class="card h-100 p-3">
-            <img src="${image}" class="card-img-top" alt="...">
+            `<div class="card h-100">
+        <img src="${image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title fw-bold">Features</h5>
-                <p class="card-text">${features}</p>
+              <h5 class="card-title">Features</h5>
+              <p class="card-text">
+              <ol>${features.map(feature => `<li>${feature}</li>`).join('')}</ol>
+              </p>
             </div>
             <div class="card-footer bg-white">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div>
-                        <h5 class="fw-bold">${name}</h5>
-                        <small class="text-muted">${published_in}</small>
-                    </div>
-                    <div>
-                        <button onclick="contentDetails('${id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#contentDetailsModal">
-                            <i class="fa-solid fa-arrow-right"></i>
-                          </button>
-                    </div>
+            <div class="d-flex justify-content-between align-items-center">
+                <div>
+                    <h5 class="fw-bold">${name}</h5>
+                    <small class="text-muted">${published_in}</small>
+                </div>
+                <div>
+                <button onclick="contentDetails('${id}')" type="button" class="btn btn-light details-btn-style" data-bs-toggle="modal" data-bs-target="#contentDetailsModal">
+                    <i class="fa-solid fa-arrow-right details-arrow"></i>
+                  </button>
                 </div>
             </div>
-    </div>`;
+          </div>`;
         dataContainer.appendChild(singleDataContainer);
     });
 }
-
 const showAllContent = () => {
     fetchData();
 }
@@ -54,13 +54,13 @@ const showAllContent = () => {
 const contentDetails = (detailsId) => {
     const url = `https://openapi.programming-hero.com/api/ai/tool/${detailsId}`
     fetch(url)
-    .then(res => res.json())
-    .then(data => showContentDetails(data.data))
+        .then(res => res.json())
+        .then(data => showContentDetails(data.data))
 }
 
-const showContentDetails = (data) =>{
+const showContentDetails = (data) => {
     console.log(data);
-    const {description, pricing} = data
+    const { description, pricing } = data
     const modeContentArea = document.getElementById('modal-content')
     modeContentArea.innerHTML = `
     <div class="row row-cols-1 row-cols-md-2 g-4 modal-main-div">
@@ -88,7 +88,6 @@ const showContentDetails = (data) =>{
     </div>
   </div>
     `
-    
 }
 
 
