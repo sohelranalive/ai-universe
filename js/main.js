4c2a01da0d68ce99f608de4b3a293797fa54c052
sohelranalive <sohel.cse37@gmail.com>
2023-03-02 21:43:59 +0600

    data fetch and primary design complete

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const fetchAllData = async () => {
+    const url = 'https://openapi.programming-hero.com/api/ai/tools';
+    const res = await fetch(url);
+    const data = await res.json();
+    showAllData(data.data.tools);
+}
+
+const showAllData = data =>{
+    const dataContainer = document.getElementById('data-container');
+    data.forEach(singleData => {
+        console.log(singleData);
+        const {image, features, name, published_in} = singleData
+        const singleDataContainer = document.createElement('div');
+        singleDataContainer.classList.add('col');
+        singleDataContainer.innerHTML = 
+        `<div class="card h-100 p-3">
+            <img src="${image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title fw-bold">Features</h5>
+                <p class="card-text">${features}</p>
+            </div>
+            <div class="card-footer bg-white">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <h5 class="fw-bold">${name}</h5>
+                        <small class="text-muted">${published_in}</small>
+                    </div>
+                    <div>
+                        <div class="arrow-circle">A</div>
+                    </div>
+                </div>
+            </div>
+    </div>`;
+    dataContainer.appendChild(singleDataContainer);
+    });
+}
+
+
+
+fetchAllData();
